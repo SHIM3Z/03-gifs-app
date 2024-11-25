642f41ba6d155f1c7f4910ab93cf08e5bf742a71
Paolo Siancas <shim3z151002@gmail.com>
2024-11-25 17:01:07 -0500

    termino de la seccion 7

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,21 +1,23 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
-  templateUrl: 'search-box.component.html'
+  templateUrl: 'search-box.component.html',
 })
-
 export class SearchBoxComponent {
-
   @ViewChild('txtTagInput')
   public tagInput!: ElementRef<HTMLInputElement>;
 
-  constructor() {
-
+  constructor(private gifsService: GifsService) {
+    // Fetch initial tags history on component initialization
   }
 
   searchTag(): void {
     const newTag = this.tagInput.nativeElement.value;
-    console.log({ newTag });
+
+    this.gifsService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value = ''; // Clear input field after search
   }
 }
